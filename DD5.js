635806edff68f4452b76312d03625f1b8b85ba52
Denis Vaumoron <dvaumoron@gmail.com>
2015-12-16 23:54:09 +0100

    generation html ability et skill, ajout sauvegarde localStorage

--- DD5.js
+++ DD5.js
@@ -54,11 +54,19 @@ $(document).ready(function() {
 		return string.charAt(0).toUpperCase() + string.slice(1);
 	}
 
-	$.each(classes, function(key, value) {
-		$('#class').append('<option value="' + key + '">' + capitalizeFirstLetter(key) + '</option>')
+	function spaceCamel(string) {
+		return string.replace(/([A-Z])/g, ' $1')
+	}
+
+	$.each(classes, function(classe, value) {
+		$('#class').append('<option value="' + classe + '">' + capitalizeFirstLetter(classe) + '</option>')
 	});
 
 	var abilityList = ['str','dex','con','int','wis','cha'];
+
+	$.each(abilityList, function(index, ability) {
+		$('#abilityTable').append('<tr><td>' + capitalizeFirstLetter(ability) + ' : </td><td><input type="number" id="' + ability + '" /></td><td><span id="' + ability + 'Mod"></span></td><td><span id="' + ability +'ST"></span></td></tr>');
+	});
 
 	var skillByAbility = {
 		str:['athletics'],
@@ -68,6 +76,20 @@ $(document).ready(function() {
 		wis:['animalHandling', 'insight', 'medecine', 'perception', 'survival'],
 		cha:['deception', 'intimidation', 'performance', 'persuasion']
 	}
+
+	var skillList = [];
+	var abilityBySkill = {}
+	$.each(abilityList, function(index, ability) {
+		$.each(skillByAbility[ability], function(index2, skill) {
+			skillList.push(skill);
+			abilityBySkill[skill] = ability;
+		});
+	});
+
+	skillList.sort();
+	$.each(skillList, function(index, skill) {
+		$('#skillTable').append('<tr><td><input type="checkbox" id="' + skill + 'Trained" /></td><td><span id="' + skill + 'Mod"></span></td><td>' + capitalizeFirstLetter(spaceCamel(skill)) + '(' + capitalizeFirstLetter(abilityBySkill[skill]) + ')</td></tr>');
+	});
 
 	function calculateMod(value, warn) {
 		var score = parseInt(value);
@@ -95,25 +117,25 @@ $(document).ready(function() {
 	function updateST() {
 		var prof = calculateProf($("#level").val(), false);
 		var stProf = classes[$('#class').val()].savingThrowProf;
-		$.each(abilityList, function(index, value) {
-			var mod = calculateMod($('#' + value).val(), false);
-			if ($.inArray(value, stProf) != -1) {
-				$('#' + value + 'ST').text(toModString(mod + prof));
+		$.each(abilityList, function(index, ability) {
+			var mod = calculateMod($('#' + ability).val(), false);
+			if ($.inArray(ability, stProf) != -1) {
+				$('#' + ability + 'ST').text(toModString(mod + prof));
 			} else {
-				$('#' + value + 'ST').text(toModString(mod));
+				$('#' + ability + 'ST').text(toModString(mod));
 			}
 		});
 	}
 
 	function updateSkill() {
-		$.each(abilityList, function(index, value) {
-			$.each(skillByAbility[value], function(index2, value2) {
-				var prof = calculateProf($("#level").val(), false);
-				var mod = calculateMod($('#' + value).val(), false);
-				if ($('#' + value2 + "Trained").prop('checked')) {
-					$('#' + value2 + "Mod").text(toModString(mod + prof))
+		var prof = calculateProf($("#level").val(), false);
+		$.each(abilityList, function(index, ability) {
+			var mod = calculateMod($('#' + ability).val(), false);
+			$.each(skillByAbility[ability], function(index2, skill) {
+				if ($('#' + skill + "Trained").prop('checked')) {
+					$('#' + skill + "Mod").text(toModString(mod + prof))
 				} else {
-					$('#' + value2 + "Mod").text(toModString(mod))
+					$('#' + skill + "Mod").text(toModString(mod))
 				}
 			});
 		});
@@ -130,37 +152,37 @@ $(document).ready(function() {
 
 	$('#con').change(updateHPMax);
 
-	$.each(abilityList, function(index, value) {
-		var el = $('#' + value);
+	$.each(abilityList, function(index, ability) {
+		var el = $('#' + ability);
 		el.change(function () {
 			var prof = calculateProf($("#level").val(), false);
 			var stProf = classes[$('#class').val()].savingThrowProf;
 			var mod = calculateMod(el.val(), true);
-			$('#' + value + 'Mod').text(toModString(mod));
-			if ($.inArray(value, stProf) != -1) {
-				$('#' + value + 'ST').text(toModString(mod + prof));
+			$('#' + ability + 'Mod').text(toModString(mod));
+			if ($.inArray(ability, stProf) != -1) {
+				$('#' + ability + 'ST').text(toModString(mod + prof));
 			} else {
-				$('#' + value + 'ST').text(toModString(mod));
+				$('#' + ability + 'ST').text(toModString(mod));
 			}
-			$.each(skillByAbility[value], function(index2, value2) {
-				if ($('#' + value2 + "Trained").prop('checked')) {
-					$('#' + value2 + "Mod").text(toModString(mod + prof))
+			$.each(skillByAbility[ability], function(index2, skill) {
+				if ($('#' + skill + "Trained").prop('checked')) {
+					$('#' + skill + "Mod").text(toModString(mod + prof))
 				} else {
-					$('#' + value2 + "Mod").text(toModString(mod))
+					$('#' + skill + "Mod").text(toModString(mod))
 				}
 			});
 		});
 		el.change();
 
-		$.each(skillByAbility[value], function(index2, value2) {
-			var el2 = $('#' + value2 + "Trained");
+		$.each(skillByAbility[ability], function(index2, skill) {
+			var el2 = $('#' + skill + "Trained");
 			el2.change(function () {
 				var prof = calculateProf($("#level").val(), false);
 				var mod = calculateMod(el.val(), false);
 				if (el2.prop('checked')) {
-					$('#' + value2 + "Mod").text(toModString(mod + prof))
+					$('#' + skill + "Mod").text(toModString(mod + prof))
 				} else {
-					$('#' + value2 + "Mod").text(toModString(mod))
+					$('#' + skill + "Mod").text(toModString(mod))
 				}
 			});
 		});
@@ -168,7 +190,7 @@ $(document).ready(function() {
 
 	var levelInput = $("#level");
 	levelInput.change(function() {
-		var level = levelInput.val()
+		var level = levelInput.val();
 		$('#profMod').text(toModString(calculateProf(level, true)));
 		$('#numberHitDice').text(level);
 	});
@@ -184,4 +206,41 @@ $(document).ready(function() {
 	classSelect.change();
 	classSelect.change(updateHPMax);
 	classSelect.change(updateST);
+
+	$('#save').click(function() {
+		var save = {};
+		save['class'] = classSelect.val();
+		save['level'] = levelInput.val();
+		$.each(abilityList, function(index, ability) {
+			save[ability] = $('#' + ability).val();
+		});
+		$.each(skillList, function(index, skill) {
+			save[skill] = $('#' + skill + 'Trained').prop('checked');
+		});
+		var name = $('#name').val();
+		localStorage.setItem(name, JSON.stringify(save));
+		alert(name + ' saved');
+	});
+	$('#load').click(function() {
+		var save = JSON.parse(localStorage.getItem($('#name').val()));
+		classSelect.val(save['class']);
+		classSelect.change();
+		levelInput.val(save['level']);
+		levelInput.change();
+		$.each(abilityList, function(index, ability) {
+			var el = $('#' + ability);
+			el.val(save[ability]);
+			el.change();
+		});
+		$.each(skillList, function(index, skill) {
+			var el = $('#' + skill + 'Trained');
+			el.prop('checked', save[skill]);
+			el.change();
+		});
+	});
+	$('#remove').click(function() {
+		var name = $('#name').val();
+		localStorage.removeItem(name);
+		alert(name + ' removed');
+	});
 });
